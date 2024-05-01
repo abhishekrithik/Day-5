289e138cac3d4d6659c6fe45202da88ba7cc37b4
abhishekrithik <abhishekrithik2017@gmail.com>
2024-05-01 23:03:45 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Day 5 TASK
+1.Create your own resume data in JSON format-Completed in [script.js](./script.js)
+2.For the above JSON, iterate over all for loops (for, for in, for of, forEach)Completed in [Script.js](./script.js)
+
+THANK YOU
+
---
+++ script.js
@@ -0,0 +1,61 @@
+let myResume={
+    "basics": {
+      "name": "Abhishek G",
+      "email": "abhishekrithik2017@gamil.com",
+      "phone": 7708767614,
+      "degree": "B.E",
+      "location": {
+        "address": "112/Manickampillai street",
+        "postalCode": 638181,
+        "city": "Namakkal",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/abhishek-g-685958219/",
+          "github":"https://github.com/Abhishek"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "The Photophilehub PVT",
+        "position": "Business Executive",
+        "startDate": "2020-11-04",
+        "endDate": "2021-08-15",
+        "summary": "i am the business executive handling various task and gaining lots of experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Sri Krishna College of Technology",
+        "department": "IT",
+        "studyType": "fulltime",
+        "batch start year": 2019,
+        "batch end year": 2023,
+        "gpa": 7.6,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript",
+        "level": "begineer",
+        "project": [
+          "Brain Malignancy Using MRI Deep Learning Algorithm"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Gym,Travel",
+      }
+    ]
+  }
+  console.log(myResume);
+  
